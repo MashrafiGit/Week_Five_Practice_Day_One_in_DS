923139b17afa1c2500ef6eaa468aae66758f55d8
MashrafiGit <mdmasrafi902@gmail.com>
2025-08-06 03:15:39 +0600

    level order traversal

---
+++ level_order_traversal.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the BinaryTreeNode class structure
+
+    template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+// vector<int> getLevelOrder(BinaryTreeNode<int> *root) {
+//     vector<int> result;
+//     if (root == NULL) return result; // Handle empty tree
+
+//     queue<BinaryTreeNode<int>*> q;
+//     q.push(root);
+
+//     while (!q.empty()) {
+//         BinaryTreeNode<int>* f = q.front();
+//         q.pop();
+
+//         result.push_back(f->val);
+
+//         if (f->left)  q.push(f->left);
+//         if (f->right) q.push(f->right);
+//     }
+
+//     return result;
+// }
